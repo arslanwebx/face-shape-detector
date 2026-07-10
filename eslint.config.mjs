f0b7c05dd9004331ae86896723823ff5dea79ee8
arslanrajput-sys <arslanrajputseo@gmail.com>
2026-07-10 07:06:39 -0400

    fixed analytics thing

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,7 @@ const eslintConfig = defineConfig([
   globalIgnores([
     // Default ignores of eslint-config-next:
     ".next/**",
+    ".edge-profile-*/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
